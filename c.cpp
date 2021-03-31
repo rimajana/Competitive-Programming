fe3ae0a69dc8c6c593314f6162378abb8098ed63
rimajana <rimajana.17@iitkgp.ac.in>
2021-03-31 16:20:24 +0530

    added c.cpp

---
+++ c.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define MOD 1e9+7
+#define fo(i, a, p) for (int i = (a); i < p; i++)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve()
+{
+     cout<< "ge"<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    //string s;
+    //getline(cin,s);
+    while (t--)
+    {
+        solve();
+    }
+}
